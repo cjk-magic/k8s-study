7423e0dd96cf1d216c1ab97da498ca30bb18ce7e
cjk-magic <139297954+cjk-magic@users.noreply.github.com>
2023-09-15 17:54:15 +0900

    Update master_node.sh

--- ch5/master_node.sh
+++ ch5/master_node.sh
@@ -15,6 +15,6 @@ echo "***master node script -- 2"
 
 
 # config for kubernetes's network 
-kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+# kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
 
 echo "***master node script -- 3"
